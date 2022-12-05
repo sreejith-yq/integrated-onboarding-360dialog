0584bb2be717ac5e98d08c382bb2f30085594ea9
Simon Scholz <simon.scholz@360dialog.com>
2022-12-05 11:39:20 +0100

    Add copy code functionality

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,11 +1,12 @@
 
 interface IButton extends React.ComponentPropsWithoutRef<"button"> {
   children: React.ReactNode;
+  outlined?: boolean
 }
 
-const Button = ({children, ...rest}: IButton) => {
+const Button = ({children, outlined, ...rest}: IButton) => {
   return(
-    <button {...rest} className="block rounded-md text-blue-600 text-sm font-base px-3 py-1 outline-none hover:text-blue-800 focus:ring-gray-600 disabled:cursor-not-allowed disabled:text-gray-300">
+    <button {...rest} className={`block rounded-md text-blue-600 text-sm font-base px-3 py-1 outline-none hover:text-blue-800 focus:ring-gray-600 disabled:cursor-not-allowed disabled:text-gray-300 flex flex-row items-center gap-2 font-base ${outlined && "border border-1 border-blue-600 hover:border-blue-800 bg-gray-50"}`}>
         {children}
     </button>
   );
