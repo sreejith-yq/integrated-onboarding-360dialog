97089002fe1b731646621aefe09e0662775511c0
Simon Scholz <simon.scholz@360dialog.com>
2024-01-25 10:51:04 +0100

    Add section to generate signup link

--- pages/index.tsx
+++ pages/index.tsx
@@ -56,6 +56,7 @@ export default function Home() {
     });
   const [callbackObject, setcallbackObject] = useState<CallbackObjectType>();
   const [copied, setCopied] = useState<boolean>(false)
+  const [urlCopied, setUrlCopied] = useState<boolean>(false);
 
   const router = useRouter();
   const { id } = router.query;
@@ -94,10 +95,12 @@ export default function Home() {
   useEffect(() => {
     const timeoutCopied = window.setTimeout(() => {
         setCopied(false)
+        setUrlCopied(false);
     }, 5000);
 
     return () => window.clearTimeout(timeoutCopied );
-  }, [copied])
+  }, [copied, urlCopied])
+
 
   const handleCallback = (callbackObject: CallbackObjectType) => {
     /* The callback function returns the client ID as well as all channel IDs, for which you're enabled to fetch the API key via the Partner API */
@@ -133,7 +136,7 @@ export default function Home() {
   }, [id]);
 
 
-  const returnQueryParameterLiteral = () => {
+  const returnQueryParameterLiteral = (url?: boolean) => {
   
     const parameters = [
       { stateVar: "email", queryParam: "email" },
@@ -152,16 +155,28 @@ export default function Home() {
       if (
         queryParametersState[v.stateVar as keyof QueryParametersType] !== ""
       ) {
-        literalStringArr.push(
-          `${v.queryParam}: "${
-            queryParametersState[v.stateVar as keyof QueryParametersType]
-          }"`
-        );
+        if (url) {
+          literalStringArr.push(
+            `${v.queryParam}=${
+              queryParametersState[v.stateVar as keyof QueryParametersType]
+            }`
+          );
+        } else {
+          literalStringArr.push(
+            `${v.queryParam}: "${
+              queryParametersState[v.stateVar as keyof QueryParametersType]
+            }"`
+          );
+        }
       }
     });
 
     if (literalStringArr.length > 0) {
-      return (`queryParameters={{\n\t${literalStringArr.join(",\n\t")}\n}}`);
+      if (url) {
+        return `${number ? "&" : "?"}${literalStringArr.join("&")}`;
+      } else {
+        return `queryParameters={{\n\t${literalStringArr.join(",\n\t")}\n}}`;
+      }
     } else {
       return ``
     }
@@ -219,6 +234,24 @@ export default function Home() {
   
     return textBase.concat( "\n/>")
   }
+
+
+  const generateSignupLink = (): string => {
+    let textBase = `https://hub.360dialog.com/dashboard/app/${partnerId ? partnerId : "{partner_id}"}/permissions`
+
+    if (number) {
+      textBase = textBase.concat(`?number="${number}"`);
+    }
+
+
+    let queryParams = returnQueryParameterLiteral(true);
+    if (queryParams !== ``) {
+      textBase = textBase.concat("", queryParams);
+    }
+
+    return textBase;
+  };
+
 
   return (
     <div className="w-screen h-screen overflow-hidden">
@@ -456,6 +489,80 @@ export default function Home() {
                         )}
                       </>
                     )}
+                  </div>
+                </div>
+              </div>
+
+              <div className="flex flex-col grow pt-6">
+                <div className="flex flex-row items-baseline justify-between w-full pb-2">
+                  <p className="text-md font-bold text-gray-700 flex-none">
+                    Signup Link
+                  </p>
+                </div>
+                <div className="relative bg-gray-50 rounded-md grow text text-gray-900 text-sm pt-8">
+                  {mounted && (
+                    <SyntaxHighlighter
+                      language="jsx"
+                      style={prism}
+                      customStyle={{ background: "transparent" }}
+                      wrapLines
+                      lineProps={{
+                        style: {
+                          wordBreak: "break-all",
+                          whiteSpace: "pre-wrap",
+                        },
+                      }}
+                      className="w-full h-full p-6 max-w-xs lg:max-w-md xl:max-w-lg 2xl:max-w-full m-0"
+                    >
+                      {generateSignupLink()}
+                    </SyntaxHighlighter>
+                  )}
+                  <div className="absolute top-3 right-3">
+                    <Button
+                      onClick={() => {
+                        navigator.clipboard.writeText(generateSignupLink());
+                        setUrlCopied(true);
+                      }}
+                      outlined
+                    >
+                      {urlCopied ? (
+                        <>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth="1.5"
+                            stroke="currentColor"
+                            className="w-4 h-4"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0118 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3l1.5 1.5 3-3.75"
+                            />
+                          </svg>
+                          Copied
+                        </>
+                      ) : (
+                        <>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth="1.5"
+                            stroke="currentColor"
+                            className="w-4 h-4"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z"
+                            />
+                          </svg>
+                          Copy URL
+                        </>
+                      )}
+                    </Button>
                   </div>
                 </div>
               </div>
